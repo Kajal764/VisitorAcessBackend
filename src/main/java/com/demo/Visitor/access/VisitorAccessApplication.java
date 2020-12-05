0a4fc1fba9c64f25156034aafb42d8eae5b2be95
kajal764 <kajaldw666@gmail.com>
2020-12-05 13:46:21 +0530

    [Kajal] Added: package structure

---
+++ src/main/java/com/demo/Visitor/access/VisitorAccessApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Visitor.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/Visitor/access/VisitorAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Visitor.access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
