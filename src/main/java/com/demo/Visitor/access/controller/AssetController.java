4957d7b69023edd98a2353bf58c914a0242e570a
Sandhyavarur <sandhya.varur@mphasis.com>
2021-01-16 20:38:37 -0800

    [Sandhya] Added : Api's for store,get requests & asset tracking

--- src/main/java/com/demo/Visitor/access/controller/AssetController.java
+++ src/main/java/com/demo/Visitor/access/controller/AssetController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.Visitor.access.exception.BusinessException;
 import com.demo.Visitor.access.model.AssetList;
+import com.demo.Visitor.access.model.AssetRequests;
 import com.demo.Visitor.access.model.VisitorRequest;
 import com.demo.Visitor.access.service.AssetService;
 
@@ -59,6 +60,58 @@ public class AssetController {
 	        List<AssetList> assetLists = assetService.getAssetRequests();
 	        if (assetLists != null)
 	            responseEntity = new ResponseEntity<>(assetLists, HttpStatus.ACCEPTED);
+	        } catch (BusinessException e) {
+	            responseEntity = new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
+	        }
+	        return responseEntity;
+	    }
+	 
+	 @PostMapping(value = "/addAssetRequest")
+	    public ResponseEntity<?> addAssetRequest(@RequestBody AssetRequests asset) {
+	        ResponseEntity<?> responseEntity = null;
+	        try {
+	            boolean success = assetService.addAssetRequest(asset);
+	            responseEntity = new ResponseEntity<>(success, HttpStatus.CREATED);
+	        } catch (BusinessException e) {
+	            responseEntity = new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
+	        }
+	        return responseEntity;
+	    }
+
+
+	  @PostMapping(value = "/approveOrRejectRequestForMovement", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	  	public ResponseEntity<?> approveOrRejectRequestForMovement(@RequestBody List<AssetRequests> assetList) {
+	        ResponseEntity<?> responseEntity = null;
+	        try {
+	            boolean success = assetService.approveAndRejectRequestsForMovement(assetList);
+	            if (success)
+	                responseEntity = new ResponseEntity<>(success, HttpStatus.ACCEPTED);
+	        } catch (BusinessException e) {
+	            responseEntity = new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
+	        }
+	        return responseEntity;
+	    }
+
+	  @GetMapping(value = "/pendingAssetRequestForMovement/{odcName}/{role}", produces = MediaType.APPLICATION_JSON_VALUE)
+	  	public ResponseEntity<?> getAssetRequestForMovement(@PathVariable String odcName,@PathVariable String role)  {
+	        ResponseEntity<?> responseEntity = null;
+	        try {
+	        List<AssetRequests> assetRequest = assetService.getAssetRequestsForMovement(odcName, role);
+	        if (assetRequest != null)
+	            responseEntity = new ResponseEntity<>(assetRequest, HttpStatus.ACCEPTED);
+	        } catch (BusinessException e) {
+	            responseEntity = new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
+	        }
+	        return responseEntity;
+	    }
+
+	  @GetMapping(value = "/pendingAssetRequestForMovement/{odcName}/{role}", produces = MediaType.APPLICATION_JSON_VALUE)
+	  	public ResponseEntity<?> getAllMovements(@PathVariable String serialNumber){
+	        ResponseEntity<?> responseEntity = null;
+	        try {
+	        List<AssetRequests> assetRequest = assetService.getAllMovements(serialNumber);
+	        if (assetRequest != null)
+	            responseEntity = new ResponseEntity<>(assetRequest, HttpStatus.ACCEPTED);
 	        } catch (BusinessException e) {
 	            responseEntity = new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
 	        }
--- src/main/java/com/demo/Visitor/access/service/AssetService.java
+++ src/main/java/com/demo/Visitor/access/service/AssetService.java
@@ -10,13 +10,18 @@ import org.springframework.stereotype.Service;
 
 import com.demo.Visitor.access.exception.BusinessException;
 import com.demo.Visitor.access.model.AssetList;
+import com.demo.Visitor.access.model.AssetRequests;
 import com.demo.Visitor.access.repository.AssetRepository;
+import com.demo.Visitor.access.repository.AssetRequestRepository;
 
 @Service
 public class AssetService {
 
 	@Autowired
 	AssetRepository assetRepository;
+	
+	@Autowired
+	AssetRequestRepository assetRequestRepository;
 
 	public boolean addAsset(AssetList asset) throws BusinessException {
 		Optional<AssetList> serialNumber = assetRepository.findBySerialNumber(asset.getSerialNumber());
@@ -60,6 +65,58 @@ public class AssetService {
 				success = true;
 		}
 		return success;
+	}
+	
+	public boolean addAssetRequest(AssetRequests asset) throws BusinessException {
+		asset.setFlag(true);
+		AssetRequests assetAdded = assetRequestRepository.save(asset);
+		if (assetAdded == null)
+			throw new BusinessException("Something went wrong!!!Please try again");
+		else
+			return true;
+	}
+
+	public List<AssetRequests> getAssetRequestsForMovement(String role, String odcName) throws BusinessException {
+		List<AssetRequests> assetRequests = null;
+		if (role == "odcManager") {
+			assetRequests = assetRequestRepository.findByStatus("Pending Approval");
+			assetRequests.removeIf(odc -> odc.getName() != odcName);
+		} else
+			assetRequests = assetRequestRepository.findByStatus("Pending Approval");
+		if (assetRequests == null)
+			throw new BusinessException("No Asset request raised");
+		else
+			return assetRequests;
+	}
+
+	public boolean approveAndRejectRequestsForMovement(List<AssetRequests> assetRequest) throws BusinessException {
+		boolean success = false;
+		for (AssetRequests asset : assetRequest) {
+			assetRequestRepository.setStatus("Approved");
+				success = true;
+		}
+		return success;
+	}
+
+	public List<AssetList> getAssetRequestsToAdd(String role, String odcName) throws BusinessException {
+			List<AssetList> assetList = null;
+			if (role == "odcManager") {
+				assetList = assetRepository.findByStatus("Pending Approval");
+				assetList.removeIf(odc -> odc.getName() != odcName);
+			} else
+				assetList = assetRepository.findByStatus("Pending Approval");
+			if (assetList == null)
+				throw new BusinessException("No Asset request raised");
+			else
+				return assetList;
+		}
+
+	public List<AssetRequests> getAllMovements(String serialNumber) throws BusinessException {
+		List<AssetRequests> assetRequests = assetRequestRepository.findBySerialNumber(serialNumber);
+		if(assetRequests == null)
+			throw new BusinessException("No information available regarding the asset");
+		else
+		   return assetRequests;
 	}
 
 }
