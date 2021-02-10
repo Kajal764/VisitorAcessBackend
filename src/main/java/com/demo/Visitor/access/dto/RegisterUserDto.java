4570fd87a1007fa1cb419631a363981d88e27ae1
Supriyakv4 <supriya.kv@mphasis.com>
2021-02-10 20:36:22 +0530

    [Supriya K V] Updated: Contact number validation

--- src/main/java/com/demo/Visitor/access/dto/RegisterUserDto.java
+++ src/main/java/com/demo/Visitor/access/dto/RegisterUserDto.java
@@ -11,7 +11,7 @@ public class RegisterUserDto {
 
     }
 
-    public RegisterUserDto(@Pattern(regexp = "^[A-Za-z]{2,}$", message = "First Name is invalid") String firstName, @Pattern(regexp = "^[A-Za-z]{2,}$", message = "Last Name is invalid") String lastName, @Pattern(regexp = "^([a-zA-Z0-9]{1,}[.]?[a-zA-Z0-9]{1,}?[@][a-zA-Z]{1,}([.][a-zA-Z]{2,4}){1,2})$", message = "Email must be valid") String email, @Pattern(regexp = "^((?=.*[A-Z])(?=.*[@#$%]).{6,})$", message = "Password Should contain One Uppercase and Symbol and greater than 6 character") String password, @Pattern(regexp = "^[0-9]\\d{6}$", message = "Employee Id should be 7 digit") String empId, @Pattern(regexp = "^([+]?[9]?[1]?[-]?[0-9]{10})$", message = "Mobile no invalid") String mobileNo, List<String> role, String managerName, List<String> odc) {
+    public RegisterUserDto(@Pattern(regexp = "^[A-Za-z]{2,}$", message = "First Name is invalid") String firstName, @Pattern(regexp = "^[A-Za-z]{2,}$", message = "Last Name is invalid") String lastName, @Pattern(regexp = "^([a-zA-Z0-9]{1,}[.]?[a-zA-Z0-9]{1,}?[@][a-zA-Z]{1,}([.][a-zA-Z]{2,4}){1,2})$", message = "Email must be valid") String email, @Pattern(regexp = "^((?=.*[A-Z])(?=.*[@#$%]).{6,})$", message = "Password Should contain One Uppercase and Symbol and greater than 6 character") String password, @Pattern(regexp = "^[0-9]\\d{6}$", message = "Employee Id should be 7 digit") String empId, @Pattern(regexp = "^([6789][0-9]{9})$|([+][9][1][-][6789][0-9]{9})$", message = "Mobile no invalid") String mobileNo, List<String> role, String managerName, List<String> odc) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -44,7 +44,7 @@ public class RegisterUserDto {
     public String empId;
 
     @Column(nullable = false)
-    @Pattern(regexp = "^([+]?[9]?[1]?[-]?[0-9]{10})$", message = "Mobile no invalid")
+    @Pattern(regexp = "^([6789][0-9]{9})$|([+][9][1][-][6789][0-9]{9})$", message = "Mobile no invalid")
     public String mobileNo;
 
     @Column(nullable = false)
